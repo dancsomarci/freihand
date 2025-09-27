914ab928db2e8773282bcd94433bc01f46a38886
Marcell Dancsó <81509197+dancsomarci@users.noreply.github.com>
2025-09-27 19:24:13 +0200

    Update view_samples.py

--- view_samples.py
+++ view_samples.py
@@ -7,21 +7,15 @@ def show_sample(base_path, idx=1):
     k_list, xyz_list = utils.load_db_annotation(base_path, "training")
 
     img = utils.read_img(idx, base_path, "training", utils.sample_version.gs)
-    msk = utils.read_msk(idx, base_path)
 
     # project camera coords to image plane
     K, xyz = k_list[idx], xyz_list[idx]
     uv = utils.project_points(xyz, K)
 
     fig = plt.figure()
-    ax1 = fig.add_subplot(121)
-    ax2 = fig.add_subplot(122)
+    ax1 = fig.add_subplot(111)
     ax1.imshow(img)
-    ax2.imshow(msk)
     utils.plot_hand(ax1, uv, order="uv")
-    utils.plot_hand(ax2, uv, order="uv")
-    ax1.axis("off")
-    ax2.axis("off")
     plt.show()
 
 
